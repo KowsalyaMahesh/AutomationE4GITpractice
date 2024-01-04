133e05ce3ce84072b9eef05baa4cbb0d9366d358
Hp <Hp@DESKTOP-PCD8EML>
2024-01-04 17:59:13 +0530

    Newsamplegitpractice is committed

---
+++ src/test/java/demo/Newsamplegitpractice.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Newsamplegitpractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Commit is successfull");
+	}
+
+}
