6a40caff5691defc8ac13e2298be5539d16f545e
Hp <Hp@DESKTOP-PCD8EML>
2024-01-05 18:38:23 +0530

    Committed 3 rd code

--- src/main/java/Objectrepo/Pushingthecode.java
+++ src/main/java/Objectrepo/Pushingthecode.java
@@ -6,6 +6,7 @@ public class Pushingthecode {
 	{
 		System.out.println("Committed new code");
 		System.out.println("Committed 2nd new code");
+		System.out.println("Committed 3rd new code");
 	}
 
 }
